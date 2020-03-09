bd8105131681a094fc9feab1818c04a0e0dc331b
aleck <292677345@qq.com>
2020-03-08 20:02:08 -0700

    add initial log

--- reload.go
+++ reload.go
@@ -1,3 +1,9 @@
 package reload
 
+import "log"
+
 //go:generate protoc --go_out=. core/tracer/message.proto
+
+func init() {
+	log.Printf("[RELOAD] reload initialized for tracing and fault injection")
+}
