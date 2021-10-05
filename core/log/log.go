2654df2b5a334905bb877717947d52e27867f89f
aleck <292677345@qq.com>
2021-10-05 14:53:37 -0700

    update log

--- core/log/log.go
+++ core/log/log.go
@@ -3,6 +3,7 @@ package log
 import (
 	"fmt"
 	"runtime"
+	"strings"
 )
 
 func caller(skip int) string {
@@ -12,6 +13,23 @@ func caller(skip int) string {
 	return fmt.Sprintf(" %s:%d\n", file, line)
 }
 
+func Caller(skip int) (string, string) {
+	pc, file, line, _ := runtime.Caller(skip)
+	f := runtime.FuncForPC(pc).Name()
+	if id := strings.Index(f, "("); id != -1 {
+		f = f[id:]
+	} else {
+		id = strings.LastIndex(f, ".")
+		f = f[id+1:]
+	}
+
+	return fmt.Sprintf("func %s()", f), fmt.Sprintf(" %s:%d\n", file, line)
+}
+
 func Logf(format string, v ...interface{}) {
-	fmt.Printf(format + caller(2), v...)
+	fmt.Printf(format+caller(2), v...)
+}
+
+func Printf(format string, v ...interface{}) {
+	fmt.Printf(format, v...)
 }
